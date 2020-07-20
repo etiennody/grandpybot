c8a1fddc1cc2fd19dfbe26b6df3b230c134ea963
etiennody <etiennody@gmail.com>
2020-07-20 22:36:32 +0200

    modify search function to process address reply

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,39 @@
+import os
+
 from flask import jsonify, render_template, request
 
 from app import app
 from app.address_parser import Parser
+from app.maps import Maps
 
 
 @app.route("/")
 def index():
-    return render_template("index.html", title="GrandPy Bot")
+    API_KEY = os.environ["GOOGLE_MAPS_API_KEY"]
+    return render_template("index.html", title="GrandPy Bot", key=API_KEY)
 
 
 @app.route("/search", methods=["POST"])
 def search():
-    message = request.form["message"]
-    fulfillment_text = Parser.address_parser(message)
-    response_text = {"message": fulfillment_text}
-    return jsonify(response_text)
+    # Initialize an object to parse the data sent in form
+    message = request.form["input_message"]
+    # Get the main place keyword
+    place = Parser.address_parser(message)
+    # Call Google Maps API
+    location = Maps().get_maps_information(place)
+
+    if location is not None:
+        address = location["formatted_address"]
+        lat = location["lat"]
+        lng = location["lng"]
+
+    else:
+        address, lat, lng = None, None, None
+
+    # Dictionary in response to the ajax request
+    response = {
+        "address": address,
+        "lng": lng,
+        "lat": lat,
+    }
+    return jsonify(response)
