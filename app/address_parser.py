4318e9b7815a619cb9919da0050417370f2d631d
etiennody <etiennody@gmail.com>
2020-07-20 22:37:52 +0200

    modify address_parser module with regex

--- app/address_parser.py
+++ app/address_parser.py
@@ -1,7 +1,5 @@
-import string
 import re
-
-from app.stopwordsfr import Stopwords
+import string
 
 
 class Parser:
@@ -10,46 +8,22 @@ class Parser:
 
     @staticmethod
     def address_parser(message):
+
         message = message.lower()
-        punctuation_replacer = str.maketrans(
-            string.punctuation, " " * len(string.punctuation)
-        )
-        message_without_punctuation = " ".join(
-            message.translate(punctuation_replacer).split()
-        ).strip()
-        filtered_message = []
-        filtered_message.extend(message_without_punctuation.split())
-        response = []
-
-        for word in filtered_message:
-            if not word in Stopwords.stopwords_fr:
-                response.append(word)
+        pattern = [
+            r".*adress d\'([^\?]*)\?",
+            r".*adresse de ([^\?]*)\?",
+            r".*c\'est où ([^\?]*)\?",
+            r".*où se trouve ([^\?]*), ?",
+            r".*où se trouve ([^\?]*)\?",
+            r".*se situe ([^\?]*)\?",
+            r"([^\?]*) tu connais l\'adresse\?",
+        ]
+        response = None
+        for p in pattern:
+            response = re.search(p, message)
+            if response:
+                break
         if not response:
-            raise ValueError("")
-        return " ".join(response)
-        
-        
-        # message = message.lower()
-        # pattern = [
-        #     r"[\s\S]*(?:(d\'|de )([^\"]*)), +",
-        #     r"[\s\S]*(?:(d\'|de )([^\"]*)) +",
-        #     r"[\s\S]*(?:(d\'|de )([^\"]*))+",
-        #     r"[\s\S]*(?:(situe )([^\"]*)) +",
-        #     r"[\s\S]*(?:(trouve )([^\"]*)) +",
-        #     r"[\s\S]*(?:(est )([^\"]*)) +",
-        #     r"[\s\S]*(?:(à )([^\"]*)) +",
-        #     r"[\s\S]*(?:(découvrir )([^\"]*)) +",
-        # ]
-        # for p in pattern:
-        #     response = re.search(p, message)
-        #     if response:
-        #         return response.group(2)
-        #     else:
-        #         None
-        # if not response:
-        #     raise ValueError("")
-        # print(x.group())
-
-# regex = r"[\s\S]*(?:(d\'|de )([^\"]*)), +
-# regex_code_postale_fr = \b[0-9]{5}\b
-# ^\d+\s[A-z]+\s[A-z]+
+            raise ValueError("Ooops, not match found")
+        return response.group(1).strip()
