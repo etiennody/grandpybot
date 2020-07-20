0b91a8f18ba439101d96234862f07445388381b6
etiennody <etiennody@gmail.com>
2020-07-20 22:34:35 +0200

    add script to display the address reply

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,33 +1,32 @@
-function submit_message(message) {
-    $.post("/search", { message: message }, handle_response);
+function submit_message(input_message) {
+    // Call the ajax view in Flask via routes.py
+    $.post('/search', { input_message: input_message }, handle_response);
 
-    function handle_response(data) {
-        // append the grandpybot response to the div
-        $('.chat-container').append(`
+    function handle_response(response) {
+        // Display the location
+        $(".chat-container").append(`
             <div class="chat-message col-md-5 offset-md-7 grandpybot-message">
-                ${data.message}
+                Bien sûr mon poussin ! La voici : ${response.address}     
             </div>
-      `)
+        `);
     }
 }
 
 $('#message-form').on('submit', function (e) {
     e.preventDefault();
     const input_message = $('#input_message').val()
-    // return if the user doesn't enter any text
+    // Return if the user doesn't enter any text
     if (!input_message) {
         return
     }
-
+    // Displays user message
     $('.chat-container').append(`
-        <div class="chat-message col-md-5 human-message">
-            ${input_message}
-        </div>
-    `)
-
-    // clear the text input 
+            <div class="chat-message col-md-5 human-message">
+                ${input_message}
+            </div>
+        `)
+    // Clear the message input 
     $('#input_message').val('')
-
-    // send the message
+    // Send the message
     submit_message(input_message)
 });
