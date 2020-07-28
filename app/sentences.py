a4b78bcef036c69615d58756f51cf8397db97f22
etiennody <etiennody@gmail.com>
2020-07-28 15:36:39 +0200

    add constant module

--- app/sentences.py
+++ app/sentences.py
@@ -1,5 +1,7 @@
 import random
 
+from app.constant import CHOICES_SENTENCES, WIKI_CHOICES_SENTENCES
+
 
 class Sentences:
     """
@@ -7,22 +9,8 @@ class Sentences:
     for address reply and for wiki reply.
     """
 
-    choices = {
-        "1": "Bien sûr mon poussin ! La voici : ",
-        "2": "No problemo ! C'est cadeau : ",
-        "3": "Tu trouveras ton bonheur ici : ",
-        "4": "Evidemment mon commandant : ",
-        "5": "Tiens, j'ai quelque chose pour toi : ",
-        "6": "Il me semble bien que c'est quelque part par-là : "
-    }
-    wiki_choices = {
-        "1": "Indépendamment des réalités courantes ou des espérances manifestes et grandes se présentent par là. ",
-        "2": "Obligé de se mettre vivement à couvert, et même ça nous paraît bête... ",
-        "3": "Seule une raison, je ne plaisante jamais, répondit le maître de poste. ",
-        "4": "Ratissé, peigné, cardé ; sans y comprendre un mot. ",
-        "5": "Interrogez tous les types imaginables... ",
-        "6": "Né à bord d'une contrée tout au diable. ",
-    }
+    choices = CHOICES_SENTENCES
+    wiki_choices = WIKI_CHOICES_SENTENCES
 
     @staticmethod
     def get_sentences():
